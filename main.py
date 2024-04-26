c88067831c66ad5c145af6c20fe341e9c91ca1b4
Andrey Mikhailov <andreymikhailov03@gmail.com>
2024-04-26 15:48:46 +0300

    задание 1

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import string
 import random
 import re
 
+def nod(a, b):
+  if (b == 0):
+    return a
+  else:
+    return nod(b, a % b)
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+print(nod(a, b))
